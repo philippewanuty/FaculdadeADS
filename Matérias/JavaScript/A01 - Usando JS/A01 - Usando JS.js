835b6525bcbe37e274266bfd4ecaca553690c581
Philippe Wanuty <philippewanuty@hotmail.com>
2023-07-25 18:14:35 -0300

    Aula 01 - Usando JavaScript
    Primeira aula do curso,  usando JavaSript com HTML e vendo no console

---
+++ Matérias/JavaScript/A01 - Usando JS/A01 - Usando JS.js
@@ -0,0 +1,10 @@
+// Perguntas e respostas
+let nome = prompt("Qual é o seu nome?")
+let idade = prompt("Qual é a sua idade?")
+let cidade = prompt("Qual é a sua cidade?")
+
+// Exibindo as respostas no console
+console.log("Respostas:")
+console.log("Nome: " + nome)
+console.log("Idade: " + idade)
+console.log("Cidade: " + cidade)
