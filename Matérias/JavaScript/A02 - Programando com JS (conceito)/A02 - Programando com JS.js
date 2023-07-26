d2ea1c632487009e876a56cc76123eaa2e76c1b8
Philippe Wanuty <philippewanuty@hotmail.com>
2023-07-26 06:54:10 -0300

    Aula 02 - update código

--- Matérias/JavaScript/A02 - Programando com JS (conceito)/A02 - Programando com JS.js
+++ Matérias/JavaScript/A02 - Programando com JS (conceito)/A02 - Programando com JS.js
@@ -1,7 +1,7 @@
 var nome = "philippe"; //variavel local ou global
 idade = "30"; //variável global
-let cidade = "Natal"// não aparece quando está dentro de um bloco
+let cidade = "Natal"; // não aparece quando está dentro de um bloco
 
-console.log (nome);
+console.log(nome);
 console.log(idade);
 console.log(cidade);
