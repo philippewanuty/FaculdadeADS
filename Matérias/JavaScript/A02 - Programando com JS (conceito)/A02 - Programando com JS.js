d6b6085c3c7e4bd739f18d60a2ddcfd8b9a9e909
Philippe Wanuty <philippewanuty@hotmail.com>
2023-07-27 10:33:20 -0300

    Aula 02 - Programando com JS
    Aprendi sobre tipos de variavels.

--- Matérias/JavaScript/A02 - Programando com JS (conceito)/A02 - Programando com JS.js
+++ Matérias/JavaScript/A02 - Programando com JS (conceito)/A02 - Programando com JS.js
@@ -1,6 +1,7 @@
 var nome = "philippe"; //variavel local ou global
-idade = "30"; //variável global
+idade = 30; //variável global
 let cidade = "Natal"; // não aparece quando está dentro de um bloco
+const numero = 100 // valor inalterável
 
 console.log(nome);
 console.log(idade);
