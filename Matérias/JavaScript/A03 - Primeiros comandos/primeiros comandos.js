94e251341ec7e385176f7878134dfce42829953b
Philippe Wanuty <philippewanuty@hotmail.com>
2023-07-27 22:45:18 -0300

    Aula 03 - Tipos de dados
    Nesta aula até o momento aprendemos sobre os tipos de dados sendo alguns deles: String, number, object dentre outros.

---
+++ Matérias/JavaScript/A03 - Primeiros comandos/primeiros comandos.js
@@ -0,0 +1,24 @@
+//* Tipos de dados *//
+
+// number
+var primeiro = 43;
+var segundo = 27;
+var soma = primeiro + segundo;
+console.log("tipo do objeto: " + typeof soma);
+
+
+//objeto
+var caixa = new Object();
+caixa.numero = 43
+caixa.frase = "Eu sou lindo"
+
+console.log("tipo do objeto: " + typeof (caixa));
+
+//string
+
+frase = "Eu sou lindo";
+console.log("tipo do objeto: " + typeof frase);
+
+
+
+
