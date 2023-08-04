2805faa52beaf9cbeadd8b80fe3457b0644b31f0
Philippe Wanuty <philippewanuty@hotmail.com>
2023-08-03 23:30:13 -0300

    Aula 05 - Avançado com Arrays

---
+++ Matérias/JavaScript/A05 - Avançado arrays/A05 - Avançado com arrays.js
@@ -0,0 +1,9 @@
+var lista = [1, 2, 3, "oi", 4, 9, 10];
+console.log(lista);
+lista.splice(3, 1,"olá"); // (x,y,z) x= será o local da celula y= será quantas celulas z= sera a substituição 
+console.log(lista);
+
+
+var listaNomes =['jefferson','eryka','philippe','jeova','eva']
+
+console.log(listaNomes.indexOf('philippe'))
