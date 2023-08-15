ef65a51339e496c85ccea786f68f796a27973685
Philippe Wanuty <philippewanuty@hotmail.com>
2023-08-15 17:10:17 -0300

    Class 09 - String
    This class i learned about strings and their methods.

---
+++ Matérias/JavaScript/C09 - Strings/String.js
@@ -0,0 +1,34 @@
+var myName = 'Philippe';
+var mySurname = ' Costa';
+
+/* Word size*/
+
+var size = myName.length;
+console.log(size);
+
+/* captalized word */
+
+var upper = myName.toUpperCase();
+console.log(upper);
+
+/* Sliced word */
+
+var nameSliced = myName.slice(0, 4);
+console.log(nameSliced);
+
+/* word included */
+
+var nameIncluded = myName.includes('phil');
+console.log(nameIncluded);
+
+/* concatenation */
+
+var fullName = myName.concat(mySurname);
+console.log(fullName);
+
+console.log(mySurname.trim())
+
+
+
+
+console.log(a)
