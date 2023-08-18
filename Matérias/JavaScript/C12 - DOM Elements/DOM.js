709b437b58a0051f0ab351ac49ebc8eb76f2eda3
Philippe Wanuty <philippewanuty@hotmail.com>
2023-08-18 18:15:17 -0300

    Class 12 - DOM
    In this class was introduced the DOM - Document Object Model.  In short  is the way to select and work with  tags , ids and class throught JS.

---
+++ Matérias/JavaScript/C12 - DOM Elements/DOM.js
@@ -0,0 +1,22 @@
+//alert('Learning DOM ');
+
+//confirm('Philippe is handsome?');
+
+//var numA = parseInt(prompt('What is the number "A"?'));
+
+//var numB = parseInt(prompt('What is the number "B"?'));
+
+//var sumAB = numA + numB;
+
+//alert(`The sum of A and B is: ${sumAB}`);
+
+function Confirm() {
+  let confirmation = confirm('Do you want to delete this item??');
+  if (confirmation) {
+    alert('You deleted this item');
+  } else {
+    alert('The item is not deleted');
+  }
+}
+
+//Catching Tags in Index.html
