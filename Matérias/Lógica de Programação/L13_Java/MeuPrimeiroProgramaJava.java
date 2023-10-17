fd344987917355de4ab1189dde83edc5b1684ddc
Philippe Wanuty <philippewanuty@hotmail.com>
2023-10-17 18:06:26 -0300

    Lesson 13 - Learning Java

---
+++ Matérias/Lógica de Programação/L13_Java/MeuPrimeiroProgramaJava.java
@@ -0,0 +1,22 @@
+class MeuPrimeiroProgramaJava
+{
+
+	public static void main (String args[])
+	{
+
+		int inteiro = 47;
+		char caracter = 'F';
+		double real = 1.65;
+		String frase = "Lucy Mari";
+		boolean VF=true;
+
+		if (VF == true) {
+
+		System.out.println("Eu sou a" + frase + "tenho" + inteiro + " anos e tenho" + real + "m de altura");
+		}
+
+		System.exit(0);
+
+
+	}
+}
