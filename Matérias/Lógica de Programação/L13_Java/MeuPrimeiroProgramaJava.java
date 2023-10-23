fb9fb13be11c595a543ef8034219c5c466189319
Philippe Wanuty <philippewanuty@hotmail.com>
2023-10-23 06:26:16 -0300

    Lesson 13 - Java introduction

--- Matérias/Lógica de Programação/L13_Java/MeuPrimeiroProgramaJava.java
+++ Matérias/Lógica de Programação/L13_Java/MeuPrimeiroProgramaJava.java
@@ -4,15 +4,15 @@ class MeuPrimeiroProgramaJava
 	public static void main (String args[])
 	{
 
-		int inteiro = 47;
-		char caracter = 'F';
-		double real = 1.65;
-		String frase = "Lucy Mari";
+		int inteiro = 30;
+		double real = 1.79;
+		String nome = "Philippe Wanuty";
+		String curso = "Analise e Desv de Sistemas";
 		boolean VF=true;
 
 		if (VF == true) {
 
-		System.out.println("Eu sou a" + frase + "tenho" + inteiro + " anos e tenho" + real + "m de altura");
+		System.out.println("Eu sou o " + nome + " tenho " + inteiro + " anos, " + real + "m de altura, e estudo o curso de " + curso);
 		}
 
 		System.exit(0);
