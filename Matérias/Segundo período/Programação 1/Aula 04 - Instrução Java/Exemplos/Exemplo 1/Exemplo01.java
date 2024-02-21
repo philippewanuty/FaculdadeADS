c618411beba4b4d76d41227225b57644162f2c51
Philippe Wanuty <philippewanuty@hotmail.com>
2024-02-21 11:31:16 -0300

    Aula 04

---
+++ Matérias/Segundo período/Programação 1/Aula 04 - Instrução Java/Exemplos/Exemplo 1/Exemplo01.java
@@ -0,0 +1,17 @@
+
+public class Exemplo01 {
+
+  public static void main(String[] args) {
+    
+    int number1, number2;
+
+    number1 = 10;
+    number2 = 10;
+
+    System.out.println(number1==number2);
+    System.out.println(number1!=number2);
+    System.out.println(number1-number2);
+
+  }
+  
+}
---
+++ Matérias/Segundo período/Programação 1/Aula 04 - Instrução Java/Exemplos/Exemplo 2 if/exemplo2.java
@@ -0,0 +1,29 @@
+
+
+public class exemplo2 {
+
+  public static void main(String[] args) {
+
+    int n1, n2 ;
+
+    n1= 10;
+    n2 = 20;
+
+    if (n1>n2) {
+      System.out.println("O numero 1 e maior que o numero dois");
+    
+    }
+    else if (n1==n2) {
+        System.out.println("O dois numeros sao iguais");
+        
+      } 
+    else {
+        System.out.println(" o numero 2 e maior");
+        
+      }
+      
+    }
+    
+  }
+  
+
