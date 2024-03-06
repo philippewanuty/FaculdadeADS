4256fed8b113bdc31b30a00d349c5658f06b25bc
Philippe Wanuty <philippewanuty@hotmail.com>
2024-03-06 08:08:21 -0300

    Aula 09 - Classe String

---
+++ Matérias/Segundo período/Programação 1/Java/Aula 09/stringBuffer.java
@@ -0,0 +1,20 @@
+
+public class stringBuffer {
+
+	
+	
+	public static void main (String[] args) {
+		
+		
+		String nome = "  Descomplica e Philippe  ";
+		
+		System.out.println(nome.substring(1,5));
+		System.out.println(nome.length());
+		System.out.println(nome.trim());
+		System.out.println(nome);
+		
+		
+				
+		
+	}
+}
