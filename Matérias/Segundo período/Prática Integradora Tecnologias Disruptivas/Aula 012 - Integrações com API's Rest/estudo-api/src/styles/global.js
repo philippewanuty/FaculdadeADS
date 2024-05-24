63a4ecbf41212b94179713d07546c9ed26b434f7
Philippe Wanuty <philippewanuty@hotmail.com>
2024-05-23 22:29:08 -0300

    Aula 12 API

---
+++ Matérias/Segundo período/Prática Integradora Tecnologias Disruptivas/Aula 012 - Integrações com API's Rest/estudo-api/src/styles/global.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    font-family: 'Roboto', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.dark.COLORS.BACKGROUND};
+  }
+`;
